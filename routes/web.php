c45a170a44e9e3baf8bc0e69231e75e97e966c9d
TaufiqAN <taufiqadi02626@gmail.com>
2025-02-15 22:08:16 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/logout', [AuthController::class, 'logout']);
